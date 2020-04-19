280846d7fe4acc967bace8af1fb087f0125f4d53
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-19 10:39:56 -0500

    Fix tab spacing

--- server/server.js
+++ server/server.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 1234;
-const {postTask, getTasks, deleteTask} = require('../db/querys.js')
+const { postTask, getTasks, deleteTask } = require("../db/querys.js");
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(bodyParser.json());
 
-app.get('/allTasks', (req, res) => {
-    getTasks((err, data) => {
-        if(err) {
-            console.log('err in getting tasks from server')
-            res.sendStatus(500)
-        } else {
-            res.send(data)
-        }
-    })
+app.get("/allTasks", (req, res) => {
+  getTasks((err, data) => {
+    if (err) {
+      console.log("err in getting tasks from server");
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
-app.post('/postTask', (req, res) => {
-    postTask(req.body.task, (err, data) => {
-        if(err){
-            console.log('err in postTask in server')
-            res.sendStatus(500)
-        } else {
-            res.send(data)
-        }
-    })
-})
+app.post("/postTask", (req, res) => {
+  postTask(req.body.task, (err, data) => {
+    if (err) {
+      console.log("err in postTask in server");
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
-app.delete('/deleteTask', (req, res) => {
-    deleteTask(req.query.task, (err, data) => {
-        if(err){
-            console.log('err in deleting task in server')
-            res.sendStatus(500)
-        } else {
-            res.send(data)
-        }
-    })
-})
+app.delete("/deleteTask", (req, res) => {
+  deleteTask(req.query.task, (err, data) => {
+    if (err) {
+      console.log("err in deleting task in server");
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`hurray, you're connected to port ${port}`);
+  console.log(`hurray, you're connected to port ${port}`);
 });
